47d88d6a013d58247c4826e4d835a3966bd3e63b
Joey Bertschler <joey.bertschler@gmail.com>
2021-08-11 17:42:25 +0200

    wip

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+require('dotenv').config()
+
 console.log('web 39 rulez')
 
+const express = require('express')
+
+const server = express()
+
+server.use(express.json()) //teaches express to parse rew bodies as JSON
+
+console.log(process.env.NODE_ENV)
+
+if (process.env.NODE_ENV === 'development') { //on Heroku machine, an env variable 
+    const cors = require('cors') //is called "NODE_ENV" 
+    server.use(cors()) //with a value of production (string)
+}
+
+const PORT = process.env.PORT || 4000
+
+server.listen(PORT, () => {
+    console.log(`listening on ${PORT}`)
+})
