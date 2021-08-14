18c9354be1bd68bfcd456ec97904ec8133948e7b
Joey Bertschler <joey.bertschler@gmail.com>
2021-08-14 12:15:19 +0200

    wip

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 require('dotenv').config()
-
-console.log('web 39 rulez')
-
+const path = require('path')
 const express = require('express')
+//console.log('web 39 rulez')
 
 const server = express()
 
 server.use(express.json()) //teaches express to parse rew bodies as JSON
+server.use(express.static(path.join(__dirname, 'client/build')))
+
 
 console.log(process.env.NODE_ENV)
 
@@ -15,8 +16,15 @@ if (process.env.NODE_ENV === 'development') { //on Heroku machine, an env variab
     server.use(cors()) //with a value of production (string)
 }
 
-server.use('*', (req, res) => {
-    res.send('<h1>success</h1>')
+//our api comes earlier in the pipeline
+server.get('/api/hello', (req,res) =>{
+    res.json({ message:'hello'})
+})
+
+//catch-all sending back just index.html
+server.get('*', (req, res) => { 
+    //res.send('<h1>success</h1>')
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
 })
 
 const PORT = process.env.PORT || 4000
